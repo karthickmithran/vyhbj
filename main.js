05a038f7f151eb888bcc20eef3e33e5071a7f749
karthickmithran <90264421+karthickmithran@users.noreply.github.com>
2022-12-10 20:47:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 song1 = "";
 song2 = "";
-
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 function preload(){
     song1 = loadSound(music1.mp3);
     song2 = loadSound(music2.mp3);
@@ -13,9 +16,30 @@ function setup(){
     video = createCapture(VIDEO);
     video.hide();
 
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 
 }
 
+function modelLoaded(){
+    console.log('Posenet is initilized');
+}
+
+function gotPoses(results){
+    if(results.length > 0)
+    {
+    console.log(results);
+    leftWristX=results[0].pose.leftWrist.x;
+    leftWristY=results[0].pose.leftWrist.y;
+    console.log(leftWristX,leftWristY);
+    
+    rightWristX=results[0].pose.rightWrist.x;
+    rightWristY=results[0].pose.rightWrist.y;
+    console.log(rightWristX,rightWristY);
+    
+    }
+    }
+    
 function draw(){
     image(video,0,0,600,500);
     
